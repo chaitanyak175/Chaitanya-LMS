f434687b39f8f7decd598c62a460350ab37276d7
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-07-03 11:51:43 +0530

    fixed the navbar

--- app/(public)/_components/Navbar.tsx
+++ app/(public)/_components/Navbar.tsx
@@ -1,11 +1,24 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/logo.png";
 import { ThemeToggle } from "@/components/ui/themeToggle";
 import { authClient } from "@/lib/auth-client";
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import UserDropdown from "./UserDropdown";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const navigationItems = [
     { name: "Home", href: "/" },
@@ -15,58 +28,143 @@ const navigationItems = [
 
 export function Navbar() {
     const { data: session, isPending } = authClient.useSession();
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    console.log("Live Pathname:", pathname);
+    console.log("Live SearchParams:", searchParams.toString());
+
+    const isActiveRoute = (href: string) => {
+        if (href === "/") {
+            return pathname === "/";
+        }
+        return pathname.startsWith(href);
+    };
 
     return (
         <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur-[backdrop-filter]:bg-background/60">
-            <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
-                <Link href="/" className="flex items-center space-x-2 mr-4">
-                    <Image src={Logo} alt="Logo" className="size-9" />
-                    <span className="font-bold">Chaitanya LMS.</span>
-                </Link>
-
-                {/* Desktop Navigation */}
-
-                <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
-                    <div className="flex items-center space-x-2">
-                        {navigationItems.map((item) => (
-                            <Link
-                                key={item.name}
-                                href={item.href}
-                                className="text-sm font-medium transition-colors hover:text-primary"
+            <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8 md:justify-between justify-between">
+                <div className="flex items-center gap-2">
+                    {/* Mobile menu trigger */}
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button
+                                className="group size-8 md:hidden"
+                                variant="ghost"
+                                size="icon"
                             >
-                                {item.name}
-                            </Link>
-                        ))}
+                                <svg
+                                    className="pointer-events-none"
+                                    width={16}
+                                    height={16}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M4 12L20 12"
+                                        className="origin-center -translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+                                    />
+                                    <path
+                                        d="M4 12H20"
+                                        className="origin-center transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+                                    />
+                                    <path
+                                        d="M4 12H20"
+                                        className="origin-center translate-y-[7px] transition-all duration-300 ease-[cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+                                    />
+                                </svg>
+                            </Button>
+                        </PopoverTrigger>
+                        <PopoverContent
+                            align="start"
+                            className="w-36 p-1 md:hidden"
+                        >
+                            <NavigationMenu className="max-w-none *:w-full">
+                                <NavigationMenuList className="flex-col items-start gap-0 md:gap-2">
+                                    {navigationItems.map((link, index) => (
+                                        <NavigationMenuItem
+                                            key={index}
+                                            className={`py-1.5 font-medium ${
+                                                isActiveRoute(link.href)
+                                                    ? "text-primary"
+                                                    : "text-muted-foreground hover:text-primary"
+                                            }`}
+                                        >
+                                            <NavigationMenuLink
+                                                href={link.href}
+                                                className="py-1.5"
+                                                active={isActiveRoute(
+                                                    link.href
+                                                )}
+                                            >
+                                                {link.name}
+                                            </NavigationMenuLink>
+                                        </NavigationMenuItem>
+                                    ))}
+                                </NavigationMenuList>
+                            </NavigationMenu>
+                        </PopoverContent>
+                    </Popover>
+                    {/* Main nav */}
+                    <div className="flex items-center gap-6">
+                        <a
+                            href="#"
+                            className="text-primary hover:text-primary/90"
+                        >
+                            <Image src={Logo} alt="Logo" className="size-9" />
+                        </a>
+                        {/* Navigation menu */}
+                        <NavigationMenu className="max-md:hidden">
+                            <NavigationMenuList className="gap-2">
+                                {navigationItems.map((link, index) => (
+                                    <NavigationMenuItem key={index}>
+                                        <NavigationMenuLink
+                                            active={isActiveRoute(link.href)}
+                                            href={link.href}
+                                            className={`py-1.5 font-medium ${
+                                                isActiveRoute(link.href)
+                                                    ? "text-primary"
+                                                    : "text-muted-foreground hover:text-primary"
+                                            }`}
+                                        >
+                                            {link.name}
+                                        </NavigationMenuLink>
+                                    </NavigationMenuItem>
+                                ))}
+                            </NavigationMenuList>
+                        </NavigationMenu>
                     </div>
+                </div>
 
-                    <div className="flex items-center space-x-4">
-                        <ThemeToggle />
-                        {isPending ? null : session ? (
-                            <UserDropdown
-                                email={session.user.email}
-                                name={session.user.name}
-                                image={session.user.image ?? undefined}
-                            />
-                        ) : (
-                            <>
-                                <Link
-                                    href="/login"
-                                    className={buttonVariants({
-                                        variant: "secondary",
-                                    })}
-                                >
-                                    Login
-                                </Link>{" "}
-                                <Link
-                                    href="/login"
-                                    className={buttonVariants()}
-                                >
-                                    Get Started
-                                </Link>
-                            </>
-                        )}
-                    </div>
-                </nav>
+                <div className="flex items-center space-x-4">
+                    <ThemeToggle />
+                    {isPending ? null : session ? (
+                        <UserDropdown
+                            email={session.user.email}
+                            name={session.user.name}
+                            image={session.user.image ?? undefined}
+                        />
+                    ) : (
+                        <>
+                            <Link
+                                href="/login"
+                                className={buttonVariants({
+                                    variant: "secondary",
+                                })}
+                            >
+                                Login
+                            </Link>
+                            <Link href="/login" className={buttonVariants()}>
+                                Get Started
+                            </Link>
+                        </>
+                    )}
+                </div>
             </div>
         </header>
     );
