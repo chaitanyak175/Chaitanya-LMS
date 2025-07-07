ed9c7f952855097c5fc4e68cfb2d78d90264ffe6
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-07-07 16:17:48 +0530

    added the theme toggle to the login screen as well

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,20 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 import Logo from "@/public/logo.png";
+import { ThemeToggle } from "@/components/ui/themeToggle";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
     return (
         <div className="relative flex min-h-svh flex-col items-center justify-center">
-            <Link
-                href="/"
-                className={buttonVariants({
-                    variant: "outline",
-                    className: "absolute top-4 left-4",
-                })}
-            >
-                <ArrowLeft className="size-4" />
-                Back
-            </Link>
+            <div className="absolute top-4 flex justify-between w-full px-4">
+                <Link
+                    href="/"
+                    className={buttonVariants({
+                        variant: "outline",
+                        className: "",
+                    })}
+                >
+                    <ArrowLeft className="size-4" />
+                    Back
+                </Link>
+                <ThemeToggle />
+            </div>
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <Link
                     className="flex items-center gap-2 self-center font-medium font-sans text-2xl"
